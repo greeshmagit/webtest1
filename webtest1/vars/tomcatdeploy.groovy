52f88066ef2f27379c88da63c6f785ae353edb07
greeshmagit <106822996+greeshmagit@users.noreply.github.com>
2022-08-23 12:38:17 +0530

    Create tomcatdeploy.groovy

---
+++ webtest1/vars/tomcatdeploy.groovy
@@ -0,0 +1,14 @@
+def call(ip,user,credId){
+     sshagent(["credId"]){
+                
+                      //copy war//
+                      sh 'scp -o StrictHostKeyChecking=no webtest1/target/webtest.war ${user}@${ip}:/opt/tomcat/webapps'
+                      
+                      //shutdown tomcat server//
+                      sh 'ssh ${user}@${ip} /opt/tomcat/bin/shutdown.sh'
+                      
+                      //startup tomcat server//
+                      sh 'ssh ${user}@${ip} /opt/tomcat/bin/startup.sh'
+                      
+                   }
+               }
