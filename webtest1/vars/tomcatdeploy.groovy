8734c818e9db01bb446e1db38ae04fe95a254dbf
greeshmagit <106822996+greeshmagit@users.noreply.github.com>
2022-08-24 15:26:11 +0530

    Update tomcatdeploy.groovy

--- webtest1/vars/tomcatdeploy.groovy
+++ webtest1/vars/tomcatdeploy.groovy
@@ -1,14 +1,14 @@
-def call(credId,user,ip) {
+def call(user,ip,credId) {
      sshagent(["credId"]) {
                 
                       //copy war//
-                      sh 'scp webtest1/target/webtest.war ${user}@${ip}:/opt/tomcat/webapps'
+                      sh "scp webtest1/target/webtest.war ${user}@${ip}:/opt/tomcat/webapps"
                       
                       //shutdown tomcat server//
-                      sh 'ssh ${user}@${ip} /opt/tomcat/bin/shutdown.sh'
+                      sh "ssh ${user}@${ip} /opt/tomcat/bin/shutdown.sh"
                       
                       //startup tomcat server//
-                      sh 'ssh ${user}@${ip} /opt/tomcat/bin/startup.sh'
+                      sh "ssh ${user}@${ip} /opt/tomcat/bin/startup.sh"
                       
                    }
                }
