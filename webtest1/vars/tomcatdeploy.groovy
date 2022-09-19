5d449bfa771079895c32e0e9b7e4f594d5449667
greeshmagit <106822996+greeshmagit@users.noreply.github.com>
2022-09-19 19:59:41 +0530

    Update tomcatdeploy.groovy

--- webtest1/vars/tomcatdeploy.groovy
+++ webtest1/vars/tomcatdeploy.groovy
@@ -4,7 +4,7 @@ def call(user,ip,credId) {
                       //copy war//
                       /*sh "scp -o StrictHostKeyChecking=no webtest1/target/webtest.war ${user}@${ip}:/opt/tomcat/webapps"
                       sh "ssh -o StrictHostkeyChecking=no ec2-user@172.31.32.18 docker run -d -p 8080:8080 --name tomcat pgreeshma/webtest:v1"*/
-                      sh "ssh -o StrictHostkeyChecking=no ${user}@${ip} docker run -d -p 8080:8080 --name tomcat pgreeshma/webtest:v1"
+                      sh "ssh -o StrictHostkeyChecking=no ${user}@${ip} docker run -d -p 8080 : 8080 --name tomcat pgreeshma/webtest:v1"
                       
                       /*shutdown tomcat server//
                       sh "ssh ${user}@${ip} /opt/tomcat/bin/shutdown.sh"
